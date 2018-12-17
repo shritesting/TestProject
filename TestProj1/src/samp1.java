fdb01e60ce9b02437181bbbfd54acf4afc0f5371
ambatisrilatha97 <ambatisrilatha97@10.0.0.221>
2018-12-16 18:03:28 -0800

    test

--- TestProj1/src/samp1.java
+++ TestProj1/src/samp1.java
@@ -4,6 +4,7 @@ public class samp1 {
 	public static void main(String[] args) {
 		System.out.println("my name is shri");
 		System.out.println("I am from india");
+		System.out.println("test");
 
 	}
 
